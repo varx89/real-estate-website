2461f4a177810415252a5c1fcb83ac5370edf964
Andrei Varcus <andrei.varcus@yahoo.com>
2022-11-16 23:23:55 +0200

    complete the header component & make it dinamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
 
 export default function Header() {
+    const [pageState, setPageState] = useState('Sign in');
     const location = useLocation();
     const navigate = useNavigate();
+
+    const auth = getAuth();
+    //check the changes of auth
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                setPageState('Profile');
+            }else{
+                setPageState('Sign in');
+            }
+        });
+    }, [auth]);
     
     const pathMatchRoute = (route) => {
         if(route === location.pathname){
@@ -22,7 +36,9 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li onClick={() => navigate('/')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && 'text-black border-b-red-500'}`}>Home</li>
                     <li onClick={() => navigate('/offers')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`}>Offers</li>
-                    <li onClick={() => navigate('/sign-in')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`}>Sign In</li>
+                    <li onClick={() => navigate('/profile')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile') ) && 'text-black border-b-red-500'}`}>
+                        {pageState}
+                    </li>
                 </ul>
             </div>
         </header>
