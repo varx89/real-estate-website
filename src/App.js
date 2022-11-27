52cae27166a0bf6a82877141f0df690133de6a1d
Andrei Varcus <andrei.varcus@yahoo.com>
2022-11-27 23:11:36 +0200

    created edit listing page and add functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing';
+import EditListing from './pages/EditListing';
 
 
 const App = () => {
@@ -28,6 +29,9 @@ const App = () => {
           <Route path='/offers' element={ <Offers /> } />
           <Route path='/create-listing' element={ <PrivateRoute /> }>
             <Route path='/create-listing' element={ <CreateListing /> } />
+          </Route>
+          <Route path='/edit-listing' element={ <PrivateRoute /> }>
+            <Route path='/edit-listing/:listingID' element={ <EditListing /> } />
           </Route>
         </Routes>
       </Router>
