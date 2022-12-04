bd3cfa89450d0a005c7844233cb723637ef76bd5
Andrei Varcus <andrei.varcus@yahoo.com>
2022-12-04 14:01:04 +0200

    fix leaflet popup

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -118,7 +118,7 @@ const Listing = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                     <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
                         <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                            {listing.address}
                         </Popup>
                     </Marker>
                 </MapContainer>
